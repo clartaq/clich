46afacba418afaec4ef27fdf4273410f00d6cb7a
clartaq <clartaq@axaluna.com>
2020-02-06 17:32:47 -0500

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
-(defproject clich "0.1.0"
+(defproject clich "0.1.1"
   :description "Clich is a demo of a simple Rich Text Editor written in ClojureScript"
-  :url "https://bitbucket.org/David_Clark/clich"
+  :url "https://github.com/clartaq/clich"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.217"]
-                 [javax.xml.bind/jaxb-api "2.3.0"]
-                 [reagent "0.7.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.597"]
+                 [reagent "0.9.1"]]
 
-  :plugins [[lein-cljsbuild "1.1.5"]
-            [lein-figwheel "0.5.15"]]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-figwheel "0.5.19"]]
 
   :min-lein-version "2.5.0"
 
   :clean-targets ^{:protect false}
+
 [:target-path
  [:cljsbuild :builds :app :compiler :output-dir]
  [:cljsbuild :builds :app :compiler :output-to]]
@@ -50,7 +50,5 @@
 
   :aliases {"package" ["do" "clean" ["cljsbuild" "once" "release"]]}
 
-  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.9"]
-                                  [figwheel-sidecar "0.5.15"]
-                                  [org.clojure/tools.nrepl "0.2.13"]
-                                  [com.cemerick/piggieback "0.2.2"]]}})
+  :profiles {:dev {:dependencies [[binaryage/devtools "1.0.0"]
+                                  [figwheel-sidecar "0.5.19"]]}})
